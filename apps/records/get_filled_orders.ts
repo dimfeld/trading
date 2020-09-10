3689e6f393dbe6bae2ad002399ba4e96cfa7319d
Daniel Imfeld <daniel@imfeld.dev>
2020-09-10 22:26:55 +0000

    Update for new API

--- apps/records/get_filled_orders.ts
+++ apps/records/get_filled_orders.ts
@@ -1,23 +1,26 @@
 #!/usr/bin/env ts-node
-import * as _ from "lodash";
-import { Api } from "tda-api";
+import * as _ from 'lodash';
+import { createBrokers } from 'trading-data';
+import { BrokerChoice } from 'types';
 
 (async () => {
+  let api = await createBrokers();
   let nextMonth = new Date();
   nextMonth.setDate(nextMonth.getDate() + 15);
 
-  let api = new Api(require("../../tda_auth.json"), false);
-  await api.init();
-  let today = new Date();
-  let day = _.padStart(today.getDate().toString(), 2, "0");
-  let month = _.padStart((today.getMonth() + 1).toString(), 2, "0");
+  let startDate = new Date();
+  startDate.setHours(0);
 
-  let startDate = `${today.getFullYear()}-${month}-${day}`;
-  let data = await api.getTrades({
+  let data = await api.getOrders(BrokerChoice.tda, {
     startDate,
+    filled: true,
     //startDate: '2020-08-27',
     //endDate: '2020-08-27',
   });
 
   console.log(JSON.stringify(data));
-})().catch(console.error);
+  return api.end();
+})().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
